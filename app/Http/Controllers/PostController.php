fb986bd89de58dc7f2e68a083b1567121a76112a
hidekiti42 <youngjoon029@gmail.com>
2026-02-13 19:00:56 +0900

    課題：投稿機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,4 +26,25 @@ class PostController extends Controller
 
         return view('posts.show', compact('posts'));
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request){
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200',
+        ]);
+
+        // フォーム入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // レダイレクトさせる
+        return redirect('/posts');
+    }
 }
